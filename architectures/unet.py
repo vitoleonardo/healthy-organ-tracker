4964dfccba720563e186139a9aa906ac80da2404
vitoleonardo <32744599+vitoleonardo@users.noreply.github.com>
2022-06-16 20:09:16 +0200

    Add more meaningfil names

--- architectures/unet.py
+++ architectures/unet.py
@@ -8,40 +8,40 @@ def conv_down(input, num_filters):
 
     x = Conv2D(num_filters, 3, padding="same")(x)
     x = BatchNormalization()(x)
-    x = Activation("relu")(x)
+    out = Activation("relu")(x)
 
-    return x
+    return out
 
 def encoder_block(input, num_filters):
-    x = conv_down(input, num_filters)
-    p = MaxPool2D((2, 2))(x)
-    return x, p
+    enc = conv_down(input, num_filters)
+    pool = MaxPool2D((2, 2))(x)
+    return enc, pool
 
 def decoder_block(input, skip_features, num_filters):
-    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
-    x = Concatenate()([x, skip_features])
-    x = conv_down(x, num_filters)
-    return x
+    up_sample = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    skip_concat = Concatenate()([up_sample, skip_features])
+    out = conv_down(skip_concat, num_filters)
+    return out
 
 def build_unet(input_shape, classes):
     inputs = Input(input_shape)
 
     # Part 1: Downsampling
-    d1, p1 = encoder_block(inputs, 64)
-    d2, p2 = encoder_block(d1, 128)
-    d3, p3 = encoder_block(d2, 256)
-    d4, p4 = encoder_block(d3, 512)
+    skip_1, enc_1 = encoder_block(inputs, 64)
+    skip_2, enc_2 = encoder_block(enc_1, 128)
+    skip_3, enc_3 = encoder_block(enc_2, 256)
+    skip_4, enc_4 = encoder_block(enc_3, 512)
 
-    bottleneck = conv_down(p4, 1024)
+    bottleneck = conv_down(enc_4, 1024)
 
     # Part 2: Upsamling
-    d1 = decoder_block(bottleneck, s4, 512)
-    d2 = decoder_block(d1, s3, 256)
-    d3 = decoder_block(d2, s2, 128)
-    d4 = decoder_block(d3, s1, 64)
+    dec_1 = decoder_block(bottleneck, skip_4, 512)
+    dec_2 = decoder_block(dec_1, skip_3, 256)
+    dec_3 = decoder_block(dec_2, skip_2, 128)
+    dec_4 = decoder_block(dec_3, skip_1, 64)
 
     # Last conf containing number of classes
-    outputs = Conv2D(classes, (1,1), padding="same", activation="sigmoid")(d4)
+    outputs = Conv2D(classes, (1,1), padding="same", activation="sigmoid")(dec_4)
 
     model = Model(inputs, outputs, name="U-Net")
     return model
