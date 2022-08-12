665ff02bdbd7459d050fdee64fca9d9fe786854a
Viktor Krawutschke <viktor.krawutschke@nerdware.dev>
2022-08-12 14:09:56 +0200

    Improve documentation and typing of some columns (due to loading from csv)

--- hpc_train_files/datapreparation.py
+++ hpc_train_files/datapreparation.py
@@ -5,18 +5,19 @@ import re
 import os
 
 
-def extract_metadata(df, train_dir, remove_faulty_cases=False, crop_df="crop_df.csv"):
+def extract_metadata(df, train_dir, remove_faulty_cases=True, crop_df="crop_df.csv", channels=3, stride=2):
     """
-    > It takes the dataframe from the train.csv file and merges it with the metadata from the slice meta
-    info
+    It takes the dataframe from the train.csv file, and merges it with the dataframe from the
+    crop_df.csv file.
     
-    :param df: the dataframe containing the segmentation data
-    :param train_dir: The directory where the training images are stored
-    :param remove_faulty_cases: This is a boolean parameter that determines whether or not to remove the
-    faulty cases, defaults to True
-    :return: A dataframe with the following columns:
-        id, large_bowel, small_bowel, stomach, case, day, slice, path, width, height, pixel_x, pixel_y,
-    count
+    :param df: the dataframe containing the training data
+    :param train_dir: the directory where the training data is stored
+    :param remove_faulty_cases: This is a boolean that determines whether or not to remove the faulty
+    cases, defaults to True (optional)
+    :param crop_df: This is the file that contains the crop coordinates for each case, defaults to
+    crop_df.csv (optional)
+    :param channels: number of channels in the image, defaults to 3 (optional)
+    :param stride: The stride of the sliding window, defaults to 2 (optional)
     """
     # Get data from train.csv
     df_train = pd.DataFrame({'id':df['id'][::3]})
@@ -25,15 +26,15 @@ def extract_metadata(df, train_dir, remove_faulty_cases=False, crop_df="crop_df.
     df_train['stomach'] = df['segmentation'][2::3].values
     df_train.reset_index(inplace=True,drop=True)
 
-    df_train["case"] = df_train["id"].apply(lambda x: re.findall("\d+",x)[0])
-    df_train["day"] = df_train["id"].apply(lambda x: re.findall("\d+",x)[1])
-    df_train["slice"] = df_train["id"].apply(lambda x: re.findall("\d+",x)[2])
+    df_train["case"] = df_train["id"].apply(lambda x: re.findall("\d+",x)[0]).astype('int64')
+    df_train["day"] = df_train["id"].apply(lambda x: re.findall("\d+",x)[1]).astype('int64')
+    df_train["slice"] = df_train["id"].apply(lambda x: re.findall("\d+",x)[2]).astype('int64')
 
     # Get data from slice meta info
     path_df = pd.DataFrame(glob(os.path.join(train_dir, "**", "*.png"), recursive=True),columns=['path'])
-    path_df['case'] = path_df['path'].apply(lambda x: re.findall("\d+",x)[1])
-    path_df['day'] = path_df['path'].apply(lambda x: re.findall("\d+",x)[2])
-    path_df['slice'] = path_df['path'].apply(lambda x: re.findall("\d+",x)[3])
+    path_df['case'] = path_df['path'].apply(lambda x: re.findall("\d+",x)[1]).astype('int64')
+    path_df['day'] = path_df['path'].apply(lambda x: re.findall("\d+",x)[2]).astype('int64')
+    path_df['slice'] = path_df['path'].apply(lambda x: re.findall("\d+",x)[3]).astype('int64')
     path_df['width'] = path_df['path'].apply(lambda x: re.findall("\d+",x)[4]).astype('int64')
     path_df['height'] = path_df['path'].apply(lambda x: re.findall("\d+",x)[5]).astype('int64')
 
@@ -46,10 +47,10 @@ def extract_metadata(df, train_dir, remove_faulty_cases=False, crop_df="crop_df.
     df_train['count'] = np.sum(df_train.iloc[:,1:4]!='',axis=1).values
     df_train.sort_values(by=['case','day','slice'],ignore_index=True, inplace=True)
 
-    df_train = _get_paths_array(df_train)
+    df_train = get_paths_array(df_train)
 
-    crop = pd.read_csv(crop_df, index_col=[0], dtype={'case':int, 'day':int, 'rs':int, 'cs':int, 're':int, 'ce':int})
-    df_train = df_train.merge(crop, how='left', on=['case','day'])
+    crop_df = pd.read_csv(crop_df, index_col=[0], dtype={'case':int, 'day':int, 'rs':int, 'cs':int, 're':int, 'ce':int})
+    df_train = df_train.merge(crop_df, how='left', on=['case','day'])
 
     print("Frame merged. Shape: {}".format(df_train.shape))
 
@@ -72,7 +73,7 @@ def remove_faulties(df_train):
     df_train = df_train[~df_train['id'].str.contains(fault1) & ~df_train['id'].str.contains(fault2)].reset_index(drop=True)
     return df_train
 
-def _get_paths_array(df_train, channels=3, stride=2):
+def get_paths_array(df_train, channels=3, stride=2):
     """
     > For each channel, we shift the path column by a certain stride, and then fill the missing values
     with the previous value
