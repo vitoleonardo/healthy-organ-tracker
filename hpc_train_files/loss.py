2c678138e1a8f7420c3e88d0419c9c7d83c4ae38
Viktor Krawutschke <viktor.krawutschke@nerdware.dev>
2022-08-18 01:41:47 +0200

    Add loss function for evaluating trained models

--- hpc_train_files/loss.py
+++ hpc_train_files/loss.py
@@ -58,3 +58,17 @@ def bce_dice_loss(y_true, y_pred):
     :return: The loss function is being returned.
     """
     return 0.6 * binary_crossentropy(tf.cast(y_true, tf.float32), y_pred) + 0.4 * dice_loss(tf.cast(y_true, tf.float32), y_pred)
+
+def dice_coef2(y_true, y_pred):
+    """
+    The function takes in two tensors, y_true and y_pred, and returns the dice coefficient.
+    This is another implementation for evaluating trained models.
+    
+    :param y_true: Ground truth values. shape = [batch_size, height, width, 1]
+    :param y_pred: The predicted output of the model
+    :return: The dice coefficient.
+    """
+    y_true_f = tf.reshape(tf.dtypes.cast(y_true, tf.float32), [-1])
+    y_pred_f = tf.reshape(tf.dtypes.cast(y_pred, tf.float32), [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    return (2. * intersection + 1.) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + 1.)
