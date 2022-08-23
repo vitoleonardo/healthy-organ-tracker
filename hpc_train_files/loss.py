69e39cd0b160ff1e46795deb2dbdae7f8e63ca23
Viktor Krawutschke <viktor.krawutschke@nerdware.dev>
2022-08-23 23:42:19 +0200

    Update loss.py
    Moved dice_coef2 to top

--- hpc_train_files/loss.py
+++ hpc_train_files/loss.py
@@ -17,6 +17,20 @@ def dice_coef(y_true, y_pred, smooth=1):
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+def dice_coef2(y_true, y_pred):
+    """
+    The function takes in two tensors, y_true and y_pred, and returns the dice coefficient.
+    This is another implementation for evaluating trained models.
+    
+    :param y_true: Ground truth values. shape = [batch_size, height, width, 1]
+    :param y_pred: The predicted output of the model
+    :return: The dice coefficient.
+    """
+    y_true_f = tf.reshape(tf.dtypes.cast(y_true, tf.float32), [-1])
+    y_pred_f = tf.reshape(tf.dtypes.cast(y_pred, tf.float32), [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    return (2. * intersection + 1.) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + 1.)
+
 def iou_coef(y_true, y_pred, smooth=1):
     """
     The function takes in two tensors, y_true and y_pred, and calculates the intersection over union
@@ -59,16 +73,3 @@ def bce_dice_loss(y_true, y_pred):
     """
     return 0.6 * binary_crossentropy(tf.cast(y_true, tf.float32), y_pred) + 0.4 * dice_loss(tf.cast(y_true, tf.float32), y_pred)
 
-def dice_coef2(y_true, y_pred):
-    """
-    The function takes in two tensors, y_true and y_pred, and returns the dice coefficient.
-    This is another implementation for evaluating trained models.
-    
-    :param y_true: Ground truth values. shape = [batch_size, height, width, 1]
-    :param y_pred: The predicted output of the model
-    :return: The dice coefficient.
-    """
-    y_true_f = tf.reshape(tf.dtypes.cast(y_true, tf.float32), [-1])
-    y_pred_f = tf.reshape(tf.dtypes.cast(y_pred, tf.float32), [-1])
-    intersection = tf.reduce_sum(y_true_f * y_pred_f)
-    return (2. * intersection + 1.) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + 1.)
